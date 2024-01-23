7a3c455b7488bb7ee32e9982129386ce1ec12982
cqxx1990 <624167284@qq.com>
2024-01-24 00:14:09 +0800

    添加运行命令

---
+++ server/pm2.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'deployer',
+      script: 'app.js',      // 修改为你的入口文件
+      instances: 1,           // 运行实例的数量
+      exec_mode: 'fork',      // 运行模式，可以选择 fork 或 cluster
+      watch: true,            // 是否监听文件变化，用于自动重启
+      env: {
+        NODE_ENV: 'production', 
+        PORT: 80
+      },
+    },
+  ],
+};
