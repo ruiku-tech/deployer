cf1584bb92f9593aae1e2e3f1f0f3cdea0ea5992
cqxx1990 <624167284@qq.com>
2024-08-09 10:54:08 +0800

    prod配置

---
+++ egg-server/config/config.prod.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+  return {
+    mongoose: {
+      client: {
+        url: "mongodb://127.0.0.1:27017/server_database",
+        options: {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          serverSelectionTimeoutMS: 50000, // 增加服务器选择超时
+          connectTimeoutMS: 100000, // 增加连接超时
+        },
+    },
+  }
+}
